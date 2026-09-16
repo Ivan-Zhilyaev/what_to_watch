85e3ab8e918cabe44dd9645e7a2947acfc054fad
Ivan Zhilyaev <iv.zhilyaev@yandex.ru>
2026-09-16 16:19:34 +0300

    async

--- opinions_app/forms.py
+++ opinions_app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
 from wtforms.validators import DataRequired, Length, Optional
+# Добавьте новый импорт класса к списку импортов.
+from flask_wtf.file import FileAllowed, MultipleFileField
+
 
 class OpinionForm(FlaskForm):
     title = StringField(
@@ -9,11 +12,26 @@ class OpinionForm(FlaskForm):
                     Length(1, 128)]
     )
     text = TextAreaField(
-        'Напишите мнение', 
+        'Напишите мнение',
         validators=[DataRequired(message='Обязательное поле')]
     )
     source = URLField(
         'Добавьте ссылку на подробный обзор фильма',
         validators=[Length(1, 256), Optional()]
+    )
+    # Допишите FileAllowed в качестве валидатора,
+    # укажите ему нужные аргументы.
+    images = MultipleFileField(
+        validators=[
+            FileAllowed(
+                # Список разрешенных расширений для файлов.
+                ['jpg', 'jpeg', 'png', 'gif', 'bmp'],
+                # Сообщение, в случае если расширение не совпадает.
+                message=(
+                    'Выберите файлы с расширением '
+                    '.jpg, .jpeg, .png, .gif или .bmp'
+                )
+            )
+        ]
     )
     submit = SubmitField('Добавить')
--- opinions_app/views.py
+++ opinions_app/views.py
@@ -5,6 +5,8 @@ from flask import abort, flash, redirect, render_template, url_for
 from . import app, db
 from .forms import OpinionForm
 from .models import Opinion
+# Импорт функции для загрузки данных.
+from .dropbox import async_upload_files_to_dropbox
 
 
 def random_opinion():
@@ -25,17 +27,23 @@ def index_view():
 
 
 @app.route('/add', methods=['GET', 'POST'])
-def add_opinion_view():
+# Допишите ключевое слово async к функции.
+async def add_opinion_view():
     form = OpinionForm()
     if form.validate_on_submit():
         text = form.text.data
         if Opinion.query.filter_by(text=text).first() is not None:
             flash('Такое мнение уже было оставлено ранее!')
             return render_template('add_opinion.html', form=form)
+        # Замените вызов синхронной функции на вызов асинхронной.
+        # Обязательно добавьте ключевое слово await, 
+        # так как функция async_upload_files_to_dropbox() асинхронная.
+        urls = await async_upload_files_to_dropbox(form.images.data)
         opinion = Opinion(
-            title=form.title.data,
-            text=text,
-            source=form.source.data
+            title=form.title.data, 
+            text=text, 
+            source=form.source.data,
+            images=urls
         )
         db.session.add(opinion)
         db.session.commit()
